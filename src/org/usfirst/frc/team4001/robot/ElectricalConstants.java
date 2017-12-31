0cc077b7815b5265cdc63f77da935e8dcd7f70b0
mrfabroa <eric.fabroa@ycdsbk12.ca>
2017-12-30 22:50:00 -0500

    Electrical Constants additions

--- src/org/usfirst/frc/team4001/robot/ElectricalConstants.java
+++ src/org/usfirst/frc/team4001/robot/ElectricalConstants.java
@@ -5,7 +5,7 @@ package org.usfirst.frc.team4001.robot;
  * ElectricalConstants is a mapping from the ports sensors and actuators are wired into
  * to a variable name. This provides flexibility changing wiring, makes checking
  * the wiring easier and significantly reduces the number of magic numbers
- * floating around.
+ * floating around.  ALL VALUES MUST BE VERIFIED off the default base code.
  */
 public class ElectricalConstants {
 	
@@ -30,5 +30,26 @@ public class ElectricalConstants {
 		public static final boolean DRIVETRAIN_FRONT_RIGHT_REVERSE		 = true;
 		public static final boolean DRIVETRAIN_REAR_LEFT_REVERSE			 = true;
 		public static final boolean DRIVETRAIN_REAR_RIGHT_REVERSE			 = true;
+		
+		
+		//this needs to be set accordingly for distance calculations
+		public static final int driveWheelRadius		 			= 3;//wheel radius in inches
+		public static final int drivePulsePerRotation 				= 1024; //encoder pulse per rotation
+		public static final double driveGearRatio 					= 1/1; //ratio between wheel and encoder
+		public static final double driveEncoderPulsePerRot 			= drivePulsePerRotation*driveGearRatio; //pulse per rotation * gear ratio
+		public static final double driveEncoderDistPerTick 			=(Math.PI*2*driveWheelRadius)/driveEncoderPulsePerRot;
+		public static final boolean leftDriveTrainEncoderReverse 	= true;
+		public static final boolean rightDriveTrainEncoderReverse 	= false;
+		
+		//***************************************************************************
+		//****************************** DRIVE ENCODERS *****************************
+		//***************************************************************************
+		
+		public static final int LEFT_DRIVE_ENCODER_A                            = 8;
+		public static final int LEFT_DRIVE_ENCODER_B                            = 9;
+		
+		public static final int RIGHT_DRIVE_ENCODER_A                           = 6;
+		public static final int RIGHT_DRIVE_ENCODER_B                           = 7;
+		
 
 }
