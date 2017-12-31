bdbd1752e46b826a8f2b47f72fb6bb0c5f1f161e
mrfabroa <eric.fabroa@ycdsbk12.ca>
2017-12-31 00:26:01 -0500

    Victor drivetrain implmentation

--- src/org/usfirst/frc/team4001/robot/Robot.java
+++ src/org/usfirst/frc/team4001/robot/Robot.java
@@ -9,6 +9,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 import org.usfirst.frc.team4001.robot.commands.ExampleCommand;
+import org.usfirst.frc.team4001.robot.subsystems.DriveTrain4Victor;
 import org.usfirst.frc.team4001.robot.subsystems.ExampleSubsystem;
 
 /**
@@ -22,6 +23,7 @@ public class Robot extends IterativeRobot {
 
 	public static final ExampleSubsystem exampleSubsystem = new ExampleSubsystem();
 	public static OI oi;
+	public static DriveTrain4Victor drive;
 
 	Command autonomousCommand;
 	SendableChooser<Command> chooser = new SendableChooser<>();
@@ -33,6 +35,8 @@ public class Robot extends IterativeRobot {
 	@Override
 	public void robotInit() {
 		oi = new OI();
+		drive = new DriveTrain4Victor();
+		
 		chooser.addDefault("Default Auto", new ExampleCommand());
 		// chooser.addObject("My Auto", new MyAutoCommand());
 		SmartDashboard.putData("Auto mode", chooser);
---
+++ src/org/usfirst/frc/team4001/robot/subsystems/DriveTrain4Victor.java
@@ -0,0 +1,65 @@
+package org.usfirst.frc.team4001.robot.subsystems;
+
+import org.usfirst.frc.team4001.robot.ElectricalConstants;
+import org.usfirst.frc.team4001.robot.commands.ArcadeDrive;
+
+import com.ctre.CANTalon;
+
+import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.Victor;
+
+/**
+ * Implementation of a basic 4 Victor motor {@link DriveTrain}
+ * @author eric
+ *@see DriveTrain
+ */
+public class DriveTrain4Victor extends DriveTrain {
+	
+	private Victor frontLeftMotor;
+	private Victor frontRightMotor;
+	private Victor rearLeftMotor;
+	private Victor rearRightMotor;
+	
+	
+	
+	public DriveTrain4Victor() {
+		
+		//Initialize motors
+		frontLeftMotor = new Victor(ElectricalConstants.DRIVETRAIN_FRONT_LEFT);
+		frontLeftMotor.setInverted(ElectricalConstants.DRIVETRAIN_FRONT_LEFT_REVERSE);
+		
+		frontRightMotor = new Victor(ElectricalConstants.DRIVETRAIN_FRONT_RIGHT);
+		frontRightMotor.setInverted(ElectricalConstants.DRIVETRAIN_FRONT_RIGHT_REVERSE);
+		
+		rearLeftMotor = new Victor(ElectricalConstants.DRIVETRAIN_REAR_LEFT);
+		rearLeftMotor.setInverted(ElectricalConstants.DRIVETRAIN_REAR_LEFT_REVERSE);
+		
+		rearRightMotor = new Victor(ElectricalConstants.DRIVETRAIN_REAR_RIGHT);
+		rearRightMotor.setInverted(ElectricalConstants.DRIVETRAIN_REAR_RIGHT_REVERSE);
+		
+		//initialize Drive Train
+		drive = new RobotDrive(frontLeftMotor, rearLeftMotor,frontRightMotor , rearRightMotor);
+				
+		
+	}
+	
+
+	@Override
+	public void initDefaultCommand() {
+		setDefaultCommand(new ArcadeDrive());
+	}
+
+	@Override
+	public void runLeftDrive(double power) {
+		frontLeftMotor.set(power);
+		rearLeftMotor.set(power);
+	}
+	
+
+	@Override
+	public void runRightDrive(double power) {
+		frontRightMotor.set(power);
+		rearRightMotor.set(power);
+	}
+
+}
